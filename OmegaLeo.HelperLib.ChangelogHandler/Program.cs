dc18f79a7ecfe1c06028dfee865fbae78dc45187
Nuno "Omega Leo" Diogo <nunodiogo@omegaleo.pt>
2026-01-28 13:18:07 +0000

    Updated workflow

--- OmegaLeo.HelperLib.ChangelogHandler/Program.cs
+++ OmegaLeo.HelperLib.ChangelogHandler/Program.cs
@@ -15,6 +15,7 @@ var changelogs = new Dictionary<string, Assembly>()
     {Path.Join(root, "OmegaLeo.HelperLib.Changelog", "CHANGELOG.md"), typeof(Changelog).Assembly},
     {Path.Join(root, "OmegaLeo.HelperLib", "CHANGELOG.md"), typeof(BenchmarkUtility).Assembly},
     {Path.Join(root, "OmegaLeo.HelperLib.Game", "CHANGELOG.md"), typeof(OmegaLeo.HelperLib.Game.Models.ConsoleCommand).Assembly},
+    {Path.Join(root, "OmegaLeo.HelperLib.Documentation", "CHANGELOG.md"), typeof(OmegaLeo.HelperLib.Documentation.Helpers.DocumentationHelperTool).Assembly},
 };
 
 foreach (var changelog in changelogs)
